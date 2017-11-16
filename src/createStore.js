ef288da80e6c2a7fa2682cf7d68ee904df9801f2
stopvoldemort <david.squires1@gmail.com>
2017-11-16 14:33:07 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,22 @@
+import { render } from './index.js';
 
+export default function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    console.log(`the state is ${state.count}`);
+    console.log(`the action is ${action.type}`);
+    render();
+  };
+
+  function getState(){
+    return state;
+  };
+
+
+  return {
+    dispatch,
+    getState
+  };
+};
